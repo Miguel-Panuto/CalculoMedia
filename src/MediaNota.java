b8371dd18da642f2be26f113bb9b9b9b13d7f0dc
Miguel Panuto <miguel.panuto@live.com>
2019-08-19 13:18:00 -0300

    Rep

---
+++ src/MediaNota.java
@@ -0,0 +1,13 @@
+public class MediaNota
+{
+    public static void main(String[] args)
+    {
+        double[] n = new double[3];
+        for (int i = 0; i < n.length; i++)
+        {
+            n[i] = MatClass.takeNumber(i);
+        }
+        double notaFinal = MatClass.media(n);
+        MatClass.apareceOsBaguiNaTela(notaFinal);
+    }
+}
